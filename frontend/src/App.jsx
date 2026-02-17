3b766907f0e27a94bc8648d15fe54b7f41b68287
justin <jadorsey@loyola.edu>
2026-02-16 23:05:28 -0500

    Started to work on U23

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import AppRoutes from './routes/index.jsx'  
+import AppRoutes from './routes/index.jsx'
+import Base from './routes/Base.jsx'
+import Header from './components/Header/Header.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
-          <AppRoutes />
+      <Header />
+      <Base />
+      <AppRoutes />
     </BrowserRouter>
   )
 }
---
+++ frontend/src/routes/Base.jsx
@@ -0,0 +1,12 @@
+import Banner from "../components/Banner/Banner";
+import Header from "../components/Header/Header";
+import HelpGrid from "../components/HelpGrid/HelpGrid";
+
+export default function Base() {
+    return(
+        <div>
+            <Banner />
+            <HelpGrid />
+        </div>
+    )
+}
