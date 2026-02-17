770e361ed9fbbfab33eab10877b62e205a3239c2
Seje7 <oseje@gmail.com>
2026-02-17 15:40:48 -0500

    Update to vite.config connects backend to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,16 @@ export default defineConfig({
       },
     }),
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      }
+    }
+  },
+
   test: {
     environment: "jsdom",
     globals: true,
