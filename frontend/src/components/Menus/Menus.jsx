073e7e632f3f94b24a70e72b1071c050355fb846
justin <jadorsey@loyola.edu>
2026-04-21 20:16:04 -0400

    Finished with the warning/error pop-ups

--- frontend/src/components/Menus/Menus.jsx
+++ frontend/src/components/Menus/Menus.jsx
@@ -1,27 +1,50 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./Menus.css"
+import { useState } from "react"
+import NoAccess from "../NoAccess/NoAccess";
 
 export default function Menus() {
+    const navigate = useNavigate();
+    const [showAccessModal, setShowAccessModal] = useState(false);
+
+    const handleProtectedClick = (path) => {
+        const token = localStorage.getItem("token");
+
+        if(!token) {
+            setShowAccessModal(true);
+            return;
+        }
+
+        navigate(path);
+    }
+
+    const closeModal = () => {
+        setShowAccessModal(false);
+    }
+
+
     return (
         <div className="menu-grid">
             <div className="menu-grid-boxes">
                 <div className="menu-box">
                     <h3>Upload Models</h3>
-                    <Link to="#" className="btn btn-primary">Import</Link>
+                    <button type="button" className="btn btn-primary" onClick={() => handleProtectedClick("#")}>Import</button>
                 </div>
                 <div className="menu-box">
                     <h3>Design Models</h3>
-                    <Link to="/design-models" className="btn btn-primary">Create</Link>
+                    <button className="btn btn-primary" onClick={() => handleProtectedClick("/design-models")}>Create</button>
                 </div>
                 <div className="menu-box">
-                    <h3>Assignments</h3>
-                    <Link to="#" className="btn btn-primary">View</Link>
+                    <h3>Sharing Data</h3>
+                    <button to="#" className="btn btn-primary" onClick={() => handleProtectedClick("#")}>View</button>
                 </div>
                 <div className="menu-box">
                     <h3>Quick Calc</h3>
-                    <Link to="/modelselection" className="btn btn-primary">Calculate</Link>
+                    <button className="btn btn-primary" onClick={() => Navigate("/modelselection")}>Calculate</button>
                 </div>
             </div>
+
+            {showAccessModal && <NoAccess setShowAccessModal={setShowAccessModal}/>}
         </div>
     )
 }
